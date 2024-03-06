f2cee5818f8753fd871037fd222153c9fe0b7f13
Onur Şimşek <posta@onursimsek.com>
2024-03-06 19:19:08 +0300

    Fix tests

--- src/Middleware/PreconditionRequest.php
+++ src/Middleware/PreconditionRequest.php
@@ -6,19 +6,19 @@ namespace OnurSimsek\Precondition\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
 use OnurSimsek\Precondition\Reflection;
 use Symfony\Component\HttpFoundation\Response;
 
 class PreconditionRequest
 {
-    public function __construct(private readonly Reflection $reflection)
+    public function __construct(private readonly Router $router, private readonly Reflection $reflection)
     {
     }
 
     public function handle(Request $request, Closure $next)
     {
-        $currentRoute = Route::getCurrentRoute();
+        $currentRoute = $this->router->getCurrentRoute();
         $controllerClass = $currentRoute->getControllerClass();
 
         if (! $controllerClass || in_array($controllerClass, ['\Illuminate\Routing\ViewController'])) {
